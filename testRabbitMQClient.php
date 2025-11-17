38cb0866ff6af812c50a8dfd868f5e3ce2276500
aag76 <aag76@njit.edu>
2025-11-17 11:37:49 -0500

    copied over code efrom mditerm branch for server handling in dmz
    Fix various issues in stock data fetching and processing.

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -4,7 +4,56 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
+$apiKey = "X06HO4GPPMMFGJJ";
+$stockSymbols = array("AAPL","GOOGL","AMZN","MSFT","TSLA");
+
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+
+function fetchStockData($symbol,$apiKey){
+	$apiURL = "https://www.alphavantage.co/query?function=TIME_SERIS_DAILY"."&symbol={$symbol}&interval=5min&apikey={$apiKey}";
+	$response = @file_get_contents($apiURL);
+	if($response === FALSE){
+		echo "data can't be reached";
+		return null;
+	}
+	$data = json_decode($response,true);
+	if(!data || isset($data['Error Meessage'])){
+		echo "Invalid data receieved";
+		return null;
+	}
+
+	$timeSeries = $data['Time Series (5mi)'] ?? null;
+	if(!timeSeries){
+		echo "No time series data found";
+		return null;
+	}
+	$latestTime = array_key_first($timeSeries);
+	$latestData = $timSeries[$latestTime];
+	return array(
+		'symbol' => $symbol,
+		'price' => $latestData['1. open'],
+		'timestamp' => $latestTime,
+	);
+}
+
+foreach($stocksymbols as $symbol){
+	echo "fetching data for {$symbol}";
+
+	$stockInfo = fetchStockData($symbol,$apiKey);
+
+	$request = array(
+		'type' = "StockData",
+		'symbol'= $stockInfo['symbol'],
+		'price'=> $stockInfo['price'],
+		'timestamp'=>$stockInfo['timestamp'],
+		'source'=>"alphavantage"
+	);
+	echo "sending {$symbol} data to broker";
+	$response = $client->send_request($request);
+	sleep(15);
+}
+
+/*	
 if (isset($argv[1]))
 {
   $msg = $argv[1];
@@ -27,4 +76,4 @@ print_r($response);
 echo "\n\n";
 
 echo $argv[0]." END".PHP_EOL;
-
+ */
